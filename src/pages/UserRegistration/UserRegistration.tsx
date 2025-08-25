73739d2f00d2e7a1e629cb37884db237684dc85b
lucamartinsb <lucabindermartins@gmail.com>
2025-08-25 20:05:34 -0300

    alterações no arquivo de estilização da tela de login para serem reaproveitadas as classes na tela de cadastro e adição dos assets para tela de cadastro

--- src/pages/UserRegistration/UserRegistration.tsx
+++ src/pages/UserRegistration/UserRegistration.tsx
@@ -1,28 +1,40 @@
 import { useState } from "react";
 import { supabase } from "../../services/supabaseClient";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import "./UserRegistration.css";
+import emailIcon from "../../assets/email.png";
+import senhaIcon from "../../assets/senha.png";
+import arrowRightOrange from "../../assets/arrowRightOrange.png";
+import arrowRightWhite from "../../assets/arrowRightWhite.png";
+import profielIcon from "../../assets/profile.png";
+import phoneIcon from "../../assets/phone.png";
 
 const UserRegistration = () => {
-  const [email, setEmail] = useState<string>("");
-  const [password, setPassword] = useState<string>("");
-  const [displayName, setDisplayName] = useState<string>("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [name, setName] = useState("");
+  const [phone, setPhone] = useState("");
   const navigate = useNavigate();
 
   const handleSignUp = async (e: React.FormEvent) => {
     e.preventDefault();
     try {
+      // Criando o usuário na autenticação
       const { error } = await supabase.auth.signUp({
         email,
         password,
+        options: {
+          data: {
+            name,
+            phone,
+          },
+        },
       });
 
       if (error) {
         throw error;
       }
-
-      alert(
-        "Cadastro realizado com sucesso! Verifique seu e-mail para confirmar a conta."
-      );
+      alert("Cadastro realizado com sucesso! Faça login para iniciar a seção."); // Cria uma alerta de confirmação
       navigate("/"); // Redireciona para a página de login
     } catch (error: any) {
       alert(error.message);
@@ -30,33 +42,81 @@ const UserRegistration = () => {
   };
 
   return (
-    <div className="user-registration-container">
-      <h2>Criar Nova Conta</h2>
+    <div className="main-container2">
+      <div className="title-container">
+        <h1>Crie sua Conta</h1>
+        <h2>Informe seus dados pessoais e de acesso</h2>
+      </div>
       <form onSubmit={handleSignUp}>
+        <h3>Perfil</h3>
         <div className="input-group">
+          <label htmlFor="name">Seu nome:</label>
+          <div className="inputContainer">
+            <img src={profielIcon} />
+            <input
+              placeholder="Seu nome completo"
+              type="text"
+              id="name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              required
+            />
+          </div>
+
+          <label htmlFor="phone">Seu telefone:</label>
+          <div className="inputContainer">
+            <img src={phoneIcon} />
+            <input
+              placeholder="(00) 00000-0000"
+              type="tel"
+              id="phone"
+              value={phone}
+              onChange={(e) => setPhone(e.target.value)}
+            />
+          </div>
         </div>
         <div className="input-group">
-          <label htmlFor="email">Seu email: </label>
-          <input
-            type="email"
-            id="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
+          <h2>Acesso</h2>
+          <label htmlFor="email">Seu email:</label>
+          <div className="inputContainer">
+            <img src={emailIcon} />
+            <input
+              placeholder="Seu e-mail de acesso"
+              type="email"
+              id="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+          </div>
+          <div className="input-group">
+            <label htmlFor="password">Sua senha:</label>
+            <div className="inputContainer">
+              <img src={senhaIcon} />
+              <input
+                placeholder="Senha de acesso"
+                type="password"
+                id="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
+            </div>
+          </div>
         </div>
-        <div className="input-group">
-          <label htmlFor="password">Sua senha: </label>
-          <input
-            type="password"
-            id="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-        </div>
-        <button type="submit">Cadastrar</button>
+        <button type="submit">
+          Cadastrar<img src={arrowRightWhite}></img>
+        </button>
       </form>
+      <div className="signup-link">
+        <p>Já tem uma conta?</p>
+        <Link id="link" to="/">
+          <button>
+            Acessar
+            <img src={arrowRightOrange} />
+          </button>
+        </Link>
+      </div>
     </div>
   );
 };
